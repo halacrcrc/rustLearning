2728ef2f7a3d1d185c83528e5ba99861f68ae9fc
halacrcrc <32725584+halacrcrc@users.noreply.github.com>
2022-02-15 22:45:08 +0800

    Delete first_rust_project directory

--- first_rust_project/target/debug/deps/first_rust_project-45a1dee639977835.d
+++
@@ -1,5 +0,0 @@
-e:\rust\first_rust_project\target\debug\deps\first_rust_project-45a1dee639977835.rmeta: src\main.rs
-
-e:\rust\first_rust_project\target\debug\deps\first_rust_project-45a1dee639977835.d: src\main.rs
-
-src\main.rs:
