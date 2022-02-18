374a7585a7900f359d77eafd511d7956669fb1ae
halacrcrc <2253453812q@gmail.com>
2022-02-18 19:43:24 +0800

    if_else

--- if_else/src/main.rs
+++ if_else/src/main.rs
@@ -5,10 +5,22 @@ fn main() {
     let _ala = ["a", "l", "a"];                   // 数组元素具有相同的数据类型且不变，数组长度固定不变；数组中仅值可变
     println!("{}", bytes[2]);
     bytes[2] = 233;
-    println!("{}", bytes[2])
+    println!("{}", bytes[2]);
 
-    //矢量 vec![T], 声明由未知类型组成的向量
-    
+    //矢量 vec![T], 声明由未知类型组成的向量                  //矢量可以增大缩小
+    let nums = vec![1,2,3];
+    println!("{:?}",nums);
+
+    let mut fruit= Vec::new();
+    fruit.push("apple");
+    fruit.push("banana");
+    fruit.push("cherry");
+    println!("{:?}",fruit);
+    fruit.pop();
+    fruit[0]= "bear";
+    println!("{:?},{:?}",fruit, fruit[0]);
+
+
     
 }
 
