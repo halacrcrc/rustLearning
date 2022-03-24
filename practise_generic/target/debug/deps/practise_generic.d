91ad6a16387bdaaef9a55f4c257b7bc93eb2872f
halacrcrc <2253453812q@gmail.com>
2022-03-24 13:44:23 +0800

    practise_generic

---
+++ practise_generic/target/debug/deps/practise_generic.d
@@ -0,0 +1,5 @@
+D:\rustLearning\practise_generic\target\debug\deps\practise_generic.exe: src\main.rs
+
+D:\rustLearning\practise_generic\target\debug\deps\practise_generic.d: src\main.rs
+
+src\main.rs:
---
+++ practise_generic/target/debug/practise_generic.d
@@ -0,0 +1,1 @@
+D:\rustLearning\practise_generic\target\debug\practise_generic.exe: D:\rustLearning\practise_generic\src\main.rs
