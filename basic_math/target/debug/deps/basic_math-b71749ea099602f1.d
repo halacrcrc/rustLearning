94c03de3ba783f8f3c0c44ce6816d4cbb17526fa
halacrcrc <32725584+halacrcrc@users.noreply.github.com>
2022-03-25 16:00:00 +0800

    Delete basic_math directory

--- basic_math/target/debug/deps/basic_math-b71749ea099602f1.d
+++
@@ -1,5 +0,0 @@
-D:\rustLearning\basic_math\target\debug\deps\basic_math-b71749ea099602f1.exe: src\lib.rs
-
-D:\rustLearning\basic_math\target\debug\deps\basic_math-b71749ea099602f1.d: src\lib.rs
-
-src\lib.rs:
